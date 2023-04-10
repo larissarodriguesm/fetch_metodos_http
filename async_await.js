4a4e6b98165a2cc5145428d4640d80494392d12e
felipe <gael.ferreira505@gmail.com>
2023-04-10 19:42:19 -0300

    Atualização

--- async_await.js
+++ async_await.js
@@ -1,3 +1,7 @@
+const header = {
+    'Content-type': 'application/json; charset=UTF-8'
+}
+
 const getPosts = async () => {
     let response = await fetch('https://jsonplaceholder.typicode.com/users');
     let data = await response.json();
@@ -17,9 +21,9 @@ const newPost = async () => {
         }
     }
 
-    const header = {
-        'Content-type': 'application/json; charset=UTF-8'
-    }
+    // const header = {
+    //     'Content-type': 'application/json; charset=UTF-8'
+    // }
 
     let response = await fetch(url, options, header)
     let data = await response.json()
@@ -27,6 +31,36 @@ const newPost = async () => {
     console.log(data)
 }
 
-newPost()
+//newPost()
+
+const updatePost = async (id, body) => {
+
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    let options = {
+        method: 'PUT',
+        body: body
+    }
+
+    let response = await fetch (url, options, header)
+    let json = await response.json()
+
+    console.log(json);
+}
+
+updatePost(2, {
+    title: 'Teste update',
+    body: 'Teste update',
+    userId: 10
+})
+
+const deletePost = id => {
+    fetch (`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'DELETE',
+    });
+    console.log('deletado!');
+};
+
+deletePost(3)
+
 
 
